0d253aa0a7a7208e8b441765b6938345dda5db5d
Atyla da Silva - SOLAB <dev.atyla@solab.tec.br>
2024-07-30 15:53:09 -0300

    Test commit

--- win_name.py
+++ win_name.py
@@ -1,17 +1,30 @@
+from os import system, listdir, rename
+from os.path import isdir
+
+
 def run():
     main_menu()
     return
+
+
+def clear():
+    system("cls")
 
 
 def wait_for_input(message):
     return input(message)
 
 
+def main_title(title: str):
+    main_title_mult = 30
+    print("*" * main_title_mult)
+    print("|" + title.center(main_title_mult - 2, " ") + "|")
+    print("*" * main_title_mult)
+
+
 def main_menu():
-    main_title_mult = 20
-    print("*" * main_title_mult)
-    print("|" + "Win_name".center(main_title_mult - 2, " ") + "|")
-    print("*" * main_title_mult)
+    clear()
+    main_title("Win_name")
 
     main_menu_options = {
         1: "[1] Full Rename",
@@ -35,11 +48,30 @@ def main_menu():
 
 
 def full_rename_menu():
-    print("TODO")
+    clear()
+    main_title("Full Rename")
+
+    directory = wait_for_input("Insert a directory: ")
+    new_name = wait_for_input("Insert the new name: ")
+
+    if isdir(directory):
+        #
+        entries = listdir(directory)
+        msg = f"\nThe following entries will be affected:\n\n{'\n'.join(entries)}\n\nProceed? [Y/N] "
+        if bool(wait_for_input(msg)):
+            for entry in entries:
+                # print(f"{directory}\\{entry}")
+                rename(f"{directory}\\{entry}", new_name)
+            wait_for_input("")
+
+    else:
+        print("Invalid or non-existing directory.")
+        wait_for_input("Exiting. Press anything to continue...")
     return
 
 
 def match_and_replace_menu():
+    clear()
     print("TODO")
     return
 
